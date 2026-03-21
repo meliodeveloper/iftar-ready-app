171658868d47e050b107807312fcaaf257e0a193
meliodeveloper <developer@meliolabs.io>
2026-03-21 19:05:10 +0000

    Update bundle ID to com.meliolabs.iftarready

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.meliodeveloper.iftarready',
+  appId: 'com.meliolabs.iftarready',
   appName: 'Iftar Ready',
   webDir: 'dist',
   ios: {
